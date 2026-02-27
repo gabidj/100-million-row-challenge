4459b78670ffd11822b7f1b57185dc23835aeea7
Gabi Suciu <gabriel.suciu@devchain.work>
2026-02-27 10:21:09 +0200

    test 1

--- app/Parser.php
+++ app/Parser.php
@@ -8,6 +8,23 @@ final class Parser
 {
     public function parse(string $inputPath, string $outputPath): void
     {
-        throw new Exception('TODO');
+        ini_set('memory_limit', '4G');
+
+        $result = [];
+
+        $handle = fopen($inputPath, 'r');
+        while (($line = fgets($handle)) !== false) {
+            [$url, $date] = explode(',', trim($line));
+            $path = parse_url($url, PHP_URL_PATH);
+            $day = substr($date, 0, 10);
+            $result[$path][$day] = ($result[$path][$day] ?? 0) + 1;
+        }
+        fclose($handle);
+
+        foreach ($result as &$days) {
+            ksort($days);
+        }
+
+        file_put_contents($outputPath, json_encode($result, JSON_PRETTY_PRINT));
     }
 }
